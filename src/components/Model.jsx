e236f6d8e16002b31a4dfc2253cdb80ff3363b04
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2024-12-03 15:53:26 +0300

    Just imported Threejs 😉

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,8 +1,24 @@
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import ModelView from './ModelView'
+import {yellowImg} from '../utils'
 
 const Model = () => {
+
+    const [size, setSize] = useState('small')
+    const [model, setModel] = useState({
+        title: 'iPhone 15 Pro in Natural Titanium',
+        color: ['#8F8A81', '#FFE7B9', '#6F6C64'],
+        img: yellowImg,
+    })
+    
+    // camera control for the model view
+
+    const cameraControlSmall = useRef()
+    const cameraControlLarge = useRef()
+
+    const small = useRef( new THREE.Group())
 
     useGSAP(()=> {
         gsap.to('#heading', {
@@ -18,7 +34,7 @@ const Model = () => {
             </h1>
             <div className='flex flex-col items-center mt-5'>
                 <div className='w-full h-[75vh] md:h-[90vh] overflow-hidden relative'>
-                    
+                   <ModelView /> 
                 </div>
             </div>
         </div>
