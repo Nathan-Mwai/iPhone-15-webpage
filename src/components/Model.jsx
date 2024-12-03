d4b51f1a69226a4d05e73a3071a3585683165d5f
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2024-12-03 16:55:06 +0300

    Now starting the model there was some few things that I still needed to set up since the model part does not work alone

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -3,6 +3,11 @@ import gsap from 'gsap'
 import React, { useRef, useState } from 'react'
 import ModelView from './ModelView'
 import {yellowImg} from '../utils'
+import {models, sizes} from '../constants'
+
+import * as THREE from 'three'
+import { Canvas } from '@react-three/fiber'
+import { View } from '@react-three/drei'
 
 const Model = () => {
 
@@ -18,7 +23,13 @@ const Model = () => {
     const cameraControlSmall = useRef()
     const cameraControlLarge = useRef()
 
+    // models
     const small = useRef( new THREE.Group())
+    const large = useRef( new THREE.Group())
+
+    //rotation
+    const [smallRotation, setSmallRotation] = useState(0)
+    const [largeRotation, setLargeRotation] = useState(0)
 
     useGSAP(()=> {
         gsap.to('#heading', {
@@ -34,7 +45,68 @@ const Model = () => {
             </h1>
             <div className='flex flex-col items-center mt-5'>
                 <div className='w-full h-[75vh] md:h-[90vh] overflow-hidden relative'>
-                   <ModelView /> 
+                <ModelView 
+                   index={1}
+                   groupRef={small}
+                   gsapType='view1'
+                   controlRef={cameraControlSmall}
+                   setRotationState={setSmallRotation}
+                   item={model}
+                   size={size}
+                   />
+                <ModelView 
+                   index={2}
+                   groupRef={large}
+                   gsapType='view2'
+                   controlRef={cameraControlLarge}
+                   setRotationState={setLargeRotation}
+                   item={model}
+                   size={size}
+                   /> 
+
+                   <Canvas
+                   className='w-full h-full'
+                   style={{
+                    position: 'fixed',
+                    left:0,
+                    top: 0,
+                    bottom: 0,
+                    right:0,
+                    overflow: 'hidden'
+                   }}
+                   eventSource={document.getElementById('root')}
+                   >
+                    <View.Port />
+                   </Canvas>
+                </div>
+
+                <div className='mx-auto w-full'>
+                    <p className='text-sm font-light text-center mb-5'>{model.title}</p>
+                    <div className='flex-center'>
+                        <ul className='color-container'>
+                            {models.map((item, i) => (
+                                <li key={i} className='w-6 h-6 rounded-full mx-2 cursor-pointer'
+                                style={{
+                                    backgroundColor: item.color[0]
+                                }}
+                                onClick={() => setModel(item)}
+                                />
+                            ))}
+                        </ul>
+
+                        <button className='size-btn-container'>
+                            {sizes.map(({label, value}) => (
+                                <span key={label} className='size-btn'
+                                style={{
+                                    backgroundColor: size === value ? 'white' : 'transparent',
+                                    color: size === value ? 'black': 'white'
+                                }}
+                                onClick={()=> setSize(value)}>
+                                    {label}
+                                </span>
+                            ))}
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
