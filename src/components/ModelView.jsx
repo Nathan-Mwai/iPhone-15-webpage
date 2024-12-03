ff9c843a27acefe25f52bb6b089f185b5a2ee8d8
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2024-12-03 17:42:48 +0300

    Now working on model but an error that crashes the entire web is occurring.

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import { PerspectiveCamera, View } from '@react-three/drei'
+import React, { Suspense } from 'react'
+import Lights from './Lights'
+// import IPhone from './IPhone'
 
-const ModelView = () => {
+const ModelView = ({index, groupRef, gsapType, controlRef, setRotationSize, size, item}) => {
   return (
-    <div>ModelView</div>
+    <View 
+    index={index}
+    id={gsapType}
+    className={`w-full h-full absolute ${index === 2 ? 'right-[-100%]' : ''}`}>
+        {/* Ambient light first source of light */}
+        <ambientLight intensity={0.3} />
+{/* The Perspective camera simulates the eye of  human like me */}
+        <PerspectiveCamera makeDefault position={[0,0,4]} />
+
+        <Lights/>
+
+        <Suspense fallback={<div>Loading----</div>}>
+        {/* <IPhone /> */}
+        </Suspense>
+    </View>
   )
 }
 
